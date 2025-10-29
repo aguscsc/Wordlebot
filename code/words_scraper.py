3a54438b54ec486af59c82742dd353070687d22e
aguscsc <aguscorreo2@gmail.com>
2025-10-29 10:12:33 -0300

    changed the scraper to fix the loading button to load all words

--- code/words_scraper.py
+++ code/words_scraper.py
@@ -1,55 +1,71 @@
-import requests
 from bs4 import BeautifulSoup as bt
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+# /--- selenium setup ---/
+
+# starts a fierfox session
+driver = webdriver.Firefox()
 
 # wordl
 URL = "https://wordraiders.com/wordle-words/"
+driver.get(URL)
 
-try:
-    #added user agent to simulate a windows pc
-    user = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
-    response = requests.get(URL, headers = user)
+# the show more button needs to be pressed 4 times so it shows all words on the list
+num_clicks = 4
 
-    #check errors
-    response.raise_for_status()
-    #parse the html
-    soup = bt(response.text, 'html.parser')
+# clicking
+for i in range(num_clicks):
+    try:
+        # find class show-more cta-btn 
+        button = driver.find_element(By.CSS_SELECTOR, ".show-more.cta-btn")
 
-    #find words
-    words = soup.find_all('li', class_="single-word common-word")
-    #print(words)
-    #word list
-    word_list = []
+        #click it
+        button.click()
+
+        #timeout so it loads
+        time.sleep(2)
+    # if the button is not found
+    except NoSuchElementException:
+        print("button wasn't found")
+    except Exception as e:
+        print("error while clicking")
+
+print("all words are loaded")
+
+#get the raw html
+page_html = driver.page_source
+
+#close the browser
+driver.quit()
+
+# parse the raw html
+soup = bt(page_html, 'html.parser')
+# filter the html to get only the words class and sub class 
+words = soup.find_all('li', class_="single-word common-word")
+
+word_list = []
+
+for element in words:
+    # find sub tag
+    sub = element.find('sub')
+    # erase sub tag 
+    if sub:
+        sub.decompose()
+    # gets the visible content of <li>
+    word = element.get_text(strip = True).lower()
     
-# Loop through each <li> element we found
-    for element in words:
-        
-        # 1. Find the <sub> tag (the part with the points)
-        sub_tag = element.find('sub')
-        
-        # 2. Check if it exists, and if so, remove it
-        if sub_tag:
-            sub_tag.decompose() # This destroys the <sub> tag
-            
-        # 3. Now, get the text. The <sub> text is gone!
-        word = element.get_text(strip=True).lower()
-        
-        # 4. Save the clean word
-        if len(word) == 5:
-            word_list.append(word)
-    if not word_list:
-        print("failed to retrieve words")
-    else:
-        #save words
-        file = "wordle_list.txt"
-        with open(file, 'w') as f:
-            for word in word_list:
-                f.write(f"{word}\n")
-#exceptions 
-except requests.exceptions.HTTPError as err:
-    print(f"HTTP error occurred: {err}")
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred while fetching the page: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+    # append word to the list
+    if len(word):
+        word_list.append(word)
+
+
+if not word_list:
+    print("failed to create list")
+else:
+    file = "word_list.txt"
+    with open(file, 'w') as f:
+        for word in word_list:
+            f.write(f"{word}\n")
